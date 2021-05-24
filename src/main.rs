eb345950cbccecee8bf47173fcd14b4f602a6500
Evan <whatscs@jointheb.org>
2021-05-23 18:53:56 -0700

    created keypress tracking and mapping to functions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 mod app;
 mod utils;
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event as CEvent, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
 use log::{debug, error, info, trace, warn};
-use std::{io, sync::mpsc};
+use std::{
+    error::Error,
+    io,
+    sync::mpsc,
+    thread,
+    time::{Duration, Instant},
+};
 use tui::backend::CrosstermBackend;
 use tui::layout::{Constraint, Direction, Layout};
 use tui::widgets::{Block, Borders, Widget};
 use tui::Terminal;
+
+enum Event<I> {
+    Input(I),
+}
 
 fn setup_logging() -> Result<(), fern::InitError> {
     // setup logging
@@ -25,15 +40,32 @@ fn setup_logging() -> Result<(), fern::InitError> {
     Ok(())
 }
 
-fn main() -> Result<(), io::Error> {
+fn main() -> Result<(), Box<dyn Error>> {
     setup_logging().expect("Setting up logging failed");
+
     // build terminal thing
-    let stdout = io::stdout();
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     info!("Created Terminal!");
 
     let (tx, rx) = mpsc::channel();
+    let tick_rate = Duration::from_millis(250);
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+        loop {
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+            if event::poll(timeout).unwrap() {
+                if let CEvent::Key(key) = event::read().unwrap() {
+                    tx.send(Event::Input(key)).unwrap();
+                }
+            }
+        }
+    });
 
     let mut md_app = app::App::new("MangaDex TUI");
 
@@ -41,6 +73,29 @@ fn main() -> Result<(), io::Error> {
 
     loop {
         terminal.draw(|f| md_app.draw(f))?;
+        match rx.recv()? {
+            Event::Input(event) => match event.code {
+                KeyCode::Char('q') => {
+                    disable_raw_mode()?;
+                    md_app.quit();
+                    execute!(
+                        terminal.backend_mut(),
+                        DisableMouseCapture,
+                        LeaveAlternateScreen,
+                    )?;
+                    terminal.show_cursor()?;
+                    break;
+                }
+                KeyCode::Char('a') => md_app.prev_tab(),
+                KeyCode::Char('d') => md_app.next_tab(),
+                _ => {}
+            },
+            _ => {}
+        }
+
+        if md_app.quit {
+            break;
+        }
     }
     Ok(())
 }
